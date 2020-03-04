27dfd7baf80a60dba4fed4576233cf0410a13365
Kevin Villalobos <kevinv15v@gmail.com>
2020-03-04 23:43:38 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,7 +35,6 @@ def select_series_title_with_most_human_characters
   ORDER BY COUNT(*) 
   DESC LIMIT 1;"
 end
-end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) as book_count from character_books 
